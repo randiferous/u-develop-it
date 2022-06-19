6745e2332a81474f77fcdba9cfd2a2453e077fe1
Daniel Y. Lee <daniel.younghwan.lee@gmail.com>
2022-06-18 21:45:08 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'GoodHabit00',
+        password: 'randiferous93',
         database: 'election'
     },
 );
